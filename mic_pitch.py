949efefe69e8290ccc27da6ba7b685b9d8f7267d
April Tan <apriltan@iastate.edu>
2021-06-14 22:04:37 -0500

    Having a hard time with different array sizes

--- mic_pitch.py
+++ mic_pitch.py
@@ -1,75 +1,3 @@
-# import pyaudio
-# import sys
-# import numpy as np
-# import aubio
-
-# # initialise pyaudio stream
-# p = pyaudio.PyAudio()
-
-# # open stream
-# buffer_size = 1024
-# pyaudio_format = pyaudio.paFloat32
-# n_channels = 1
-# samplerate = 44100
-# stream = p.open(format=pyaudio_format,
-#                 channels=n_channels,
-#                 rate=samplerate,
-#                 input=True,
-#                 frames_per_buffer=buffer_size)
-# hop_s = 512
-
-# if len(sys.argv) > 1:
-#     # record 5 seconds
-#     output_filename = sys.argv[1]
-#     record_duration = 5 # exit 1
-#     outputsink = aubio.sink(sys.argv[1], samplerate)
-#     total_frames = 0
-# else:
-#     # run forever
-#     outputsink = None
-#     record_duration = None
-
-# # setup pitch
-# tolerance = 0.8
-# win_s = 4096 # fft size
-# hop_s = buffer_size # hop size
-# pitch_o = aubio.pitch("default", win_s, hop_s, samplerate)
-# pitch_o.set_unit("Hz")
-# pitch_o.set_tolerance(tolerance)
-
-# pitch_user = []
-
-# print("*** starting recording. Hit Ctrl+C to stop.")
-# while True:
-#     try:
-#         audiobuffer = stream.read(buffer_size)
-#         signal = np.fromstring(audiobuffer, dtype=np.float32)
-
-# # Figure out how to add 
-
-#         pitch_user.append(pitch_o(signal)[0])
-#         # confidence = pitch_o.get_confidence()
-
-#         # print("{} / {}".format(pitch,confidence))
-
-#         if outputsink:
-#             outputsink(signal, len(signal))
-
-#         if record_duration:
-#             total_frames += len(signal)
-#             if record_duration * samplerate < total_frames:
-#                 break
-#     except KeyboardInterrupt:
-#         print("*** Ctrl+C pressed, exiting")
-#         break
-
-# print("*** done recording")
-# stream.stop_stream()
-# stream.close()
-# p.terminate()
-# print(pitch_user)
-
-
 import pyaudio
 import wave
 
@@ -113,6 +41,7 @@ wf.close()
 import sys
 from aubio import source, pitch
 from glob import glob
+
 print(glob("*"))
 filename = r"output.wav"
 
@@ -151,6 +80,8 @@ if 0: sys.exit(0)
 
 # Prep for plotting
 from numpy import array, ma
+import numpy as np
+from model_pitch import cleaned_pitches
 
 skip = 1
 
@@ -159,9 +90,12 @@ mic_confidences = array(mic_confidences[skip:])
 
 
 # plot cleaned up pitches
-cleaned_mic_pitches = mic_pitches
+cleaned_mp = mic_pitches
 
 # do not plot pitch == 0 Hz
-cleaned_mic_pitches = ma.masked_where((cleaned_mic_pitches <= 0) | (cleaned_mic_pitches <= tolerance), cleaned_mic_pitches)
+cleaned_mp = ma.masked_where((cleaned_mp <= 0) | (cleaned_mp <= tolerance), cleaned_mp)
 
-print(cleaned_mic_pitches)
+# pad array so same size
+pad_mic = np.zeros(cleaned_pitches.shape)
+pad_mic[:cleaned_mp.shape[0], :cleaned_mp.shape[1]] = cleaned_mp
+print(pad_mic)
